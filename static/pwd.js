11db13427482fab2eb71a68c7a1bd3d9aabe8e1e
Workingbotf <122946223+Workingbotf@users.noreply.github.com>
2023-01-25 16:55:01 -0600

    Create pwd.js

---
+++ static/pwd.js
@@ -0,0 +1,23 @@
+function rickrollormathwebsite() {
+    randomnumber = Math.floor(Math.random() * 10) - 1
+    if (randomnumber <= 2) {
+        window.location.href = "https://www.mathplayground.com/logic_pingu_and_friends.html";
+    } else {
+        window.location.href = "https://mathlibs.onrender.com/gotem";
+    }
+}
+if (localStorage.getItem("auth") != 'true') {
+    PWD = 'lol'
+    a = prompt("Password")
+    if (a == PWD) {
+        alert("You are signed in! DO NOT BOOKMARK THIS TAB")
+        //if password is right, store it in localstorage
+        localStorage.setItem("auth", "true")
+    } else {
+        //else rickroll or lead to math website
+        rickrollormathwebsite();
+    }
+}
+checkIfWebsiteLooksOld();
+
+
