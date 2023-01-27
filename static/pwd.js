bc8e148fc3365db1dd504066ab4afbde62923ca1
Workingbotf <122946223+Workingbotf@users.noreply.github.com>
2023-01-27 15:15:46 -0600

    Uploaded new pwd

--- static/pwd.js
+++ static/pwd.js
@@ -1,16 +1,11 @@
 function rickrollormathwebsite() {
-    randomnumber = Math.floor(Math.random() * 10) - 1
-    if (randomnumber <= 2) {
-        window.location.href = "https://www.mathplayground.com/logic_pingu_and_friends.html";
-    } else {
-        window.location.href = "https://mathlibs.onrender.com/gotem";
-    }
+    window.location.href = "https://mathlibs.onrender.com/gotem";
 }
 if (localStorage.getItem("auth") == 'true') {
     localStorage.setItem("auth", "false")
 }
 
-PWD = 'iwillcomeandgetyou'
+PWD = 'yimmyjimmy'
 a = prompt("Password")
 if (a == PWD) {
     alert("You are signed in! DO NOT BOOKMARK THIS TAB")
