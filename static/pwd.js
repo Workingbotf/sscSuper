92e7209012ecc56a5e4c4793cba33497bde7c3f3
Workingbotf <122946223+Workingbotf@users.noreply.github.com>
2023-01-27 15:16:16 -0600

    Made it more efficient

--- static/pwd.js
+++ static/pwd.js
@@ -1,16 +1,11 @@
 function rickrollormathwebsite() {
     window.location.href = "https://mathlibs.onrender.com/gotem";
 }
-if (localStorage.getItem("auth") == 'true') {
-    localStorage.setItem("auth", "false")
-}
 
 PWD = 'yimmyjimmy'
 a = prompt("Password")
 if (a == PWD) {
     alert("You are signed in! DO NOT BOOKMARK THIS TAB")
-    //if password is right, store it in localstorage
-    localStorage.setItem("auth", "true")
 } else {
     //else rickroll or lead to math website
     rickrollormathwebsite();
