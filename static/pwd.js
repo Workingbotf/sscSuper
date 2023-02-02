b52b7c0e89afbbe24e771861860daf27c861cde2
Workingbotf <122946223+Workingbotf@users.noreply.github.com>
2023-02-01 21:09:04 -0600

    made updates to pwd
    it may not work may come to fix back later

--- static/pwd.js
+++ static/pwd.js
@@ -1,14 +1,25 @@
-function rickrollormathwebsite() {
-    window.location.href = "https://mathlibs.onrender.com/gotem";
-}
+dc = (str) => {
 
-PWD = 'yimmyjimmy'
+  function _0x4705(_0x2c40ee, _0x20835d) {
+    var _0x141f6a = _0x141f();return ((_0x4705 = function (_0x470507, _0x415250) {_0x470507 = _0x470507 - 0x15d;var _0x214854 = _0x141f6a[_0x470507];return _0x214854;}),_0x4705(_0x2c40ee, _0x20835d) );}var _0xa76e5b = _0x4705;function _0x141f() {var _0x1065e2 = [
+      "4428712iuWOZQ",
+      "5KwePYY",
+      "30rVtZkK",
+      "2579604UpDJae",
+      "143862kaUhGK",
+      "183592bOGniL",
+      "353711MISaWK",
+      "315qEqXCB",
+      "1363538ubzTQQ",
+      "atob",
+      "6744303LTKcWe",];_0x141f = function () {return _0x1065e2;};return _0x141f();}(function (_0x34a04d, _0x51f92b) {var _0x5e956c = _0x4705,_0x33f996 = _0x34a04d();while (!![]) {try {var _0x96553 =parseInt(_0x5e956c(0x162)) / 0x1 +parseInt(_0x5e956c(0x164)) / 0x2 +-parseInt(_0x5e956c(0x160)) / 0x3 +(parseInt(_0x5e956c(0x167)) / 0x4) *(-parseInt(_0x5e956c(0x15d)) / 0x5) +-parseInt(_0x5e956c(0x15f)) / 0x6 +(-parseInt(_0x5e956c(0x163)) / 0x7) *(parseInt(_0x5e956c(0x161)) / 0x8) +(parseInt(_0x5e956c(0x166)) / 0x9) *(parseInt(_0x5e956c(0x15e)) / 0xa);if (_0x96553 === _0x51f92b) break;else _0x33f996["push"](_0x33f996["shift"]());} catch (_0x289ddd) { _0x33f996["push"](_0x33f996["shift"]());}}})(_0x141f, 0xa28d2);return window[_0xa76e5b(0x165)](decodeURI(str));
+};
 a = prompt("Password")
-if (a == PWD) {
+if (a == dc("ZGluZ2RpbmcxMjNA")) {
     alert("You are signed in! DO NOT BOOKMARK THIS TAB")
 } else {
     //else rickroll or lead to math website
-    rickrollormathwebsite();
+    window.location.href = "https://mathlibs.onrender.com/gotem";
 }
 
 
